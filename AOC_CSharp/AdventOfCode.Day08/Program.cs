0ffcf8dc4596bf7572e2e0e832579631e3cd8ee9
dbiro <birodani897@gmail.com>
2018-12-10 22:19:16 +0100

    Tree and Node class for Day08 solver

--- AOC_CSharp/AdventOfCode.Day08/Program.cs
+++ AOC_CSharp/AdventOfCode.Day08/Program.cs
@@ -1,7 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AdventOfCode.Day08
 {
+    class Node
+    {
+        public List<Node> Children { get; }
+        public List<int> Metadata { get; }
+
+        public Node()
+        {
+            Children = new List<Node>();
+            Metadata = new List<int>();
+        }
+    }
+    class Tree
+    {
+        public Node Root { get; }
+
+        public Tree(Node root)
+        {
+            Root = root;
+        }
+    }
+
+    //static void ReadInput()
+    //{
+    //    string input = File.ReadAllText("input.txt");
+    //    string[] inputNumbers = input.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+    //    for (int i = 0; i < inputNumbers.Length;)
+    //    {
+    //        int childrenCount = int.Parse(inputNumbers[i]);
+    //        int metadataCount = int.Parse(inputNumbers[i]);
+
+    //        Node node = new Node();
+    //    }
+    //}    
+
     class Program
     {
         static void Main(string[] args)
