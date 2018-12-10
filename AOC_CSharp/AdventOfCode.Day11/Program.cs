050be9a4c89aaddcf7d1cbfd9c445108bd13ea1a
dbiro <birodani897@gmail.com>
2018-12-10 21:51:01 +0100

    Day08-09-11 skeleton

---
+++ AOC_CSharp/AdventOfCode.Day11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode.Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
