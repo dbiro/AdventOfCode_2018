71232aa625af30497732949acfdaada88c3787f6
Biró Dániel <biro.daniel@nexogen.hu>
2018-12-12 20:29:38 +0100

    Day12 - still not working

--- AOC_CSharp/AdventOfCode.Day12/Program.cs
+++ AOC_CSharp/AdventOfCode.Day12/Program.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode.Day12
 {
     class Program
     {
-        const string INITIAL_STATE = "...#..#.#..##......###...###.....................";
+        const string INITIAL_STATE = "...#..#.#..##......###...###...........";
         static readonly IEnumerable<(string currentState, string nextState)> rules = new (string currentState, string nextState)[]
         {
             (currentState: @"\.\.\.##", nextState: "..#.."),
@@ -33,13 +33,26 @@ namespace AdventOfCode.Day12
 
             for (int i = 0; i < 20; i++)
             {
+                List<int> plantIndices = new List<int>();
                 foreach (var rule in rules)
                 {
                     Regex ruleMatcher = new Regex(rule.currentState);
-                    finalState = ruleMatcher.Replace(finalState, rule.nextState);
+                    foreach (Match match in ruleMatcher.Matches(finalState))
+                    {
+                        if (match.Success)
+                        {
+                            plantIndices.Add(match.Index + 2);
+                        }
+                    }
                 }
+                char[] stateArray = new string('.', finalState.Length).ToCharArray();
+                for (int p = 0; p < plantIndices.Count; p++)
+                {
+                    stateArray[plantIndices[p]] = '#';
+                }
+                finalState = new string(stateArray);
                 Console.WriteLine(finalState);
-            }            
+            }
         }
     }
 }
