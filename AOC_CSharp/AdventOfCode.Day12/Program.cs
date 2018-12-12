ce64d3647dc2ee85201d9c58d9811b6671fe9e24
dbiro <birodani897@gmail.com>
2018-12-12 23:02:45 +0100

    Day12 solver - part1

--- AOC_CSharp/AdventOfCode.Day12/Program.cs
+++ AOC_CSharp/AdventOfCode.Day12/Program.cs
@@ -7,52 +7,118 @@ namespace AdventOfCode.Day12
 {
     class Program
     {
-        const string INITIAL_STATE = "...#..#.#..##......###...###...........";
-        static readonly IEnumerable<(string currentState, string nextState)> rules = new (string currentState, string nextState)[]
+        const string TEST_INITIAL_STATE = "#..#.#..##......###...###";
+        const string INITIAL_STATE = "#.##.###.#.##...##..#..##....#.#.#.#.##....##..#..####..###.####.##.#..#...#..######.#.....#..##...#";
+
+        static readonly HashSet<string> TEST_RULE_SET = new HashSet<string>()
         {
-            (currentState: @"\.\.\.##", nextState: "..#.."),
-            (currentState: @"\.\.#\.\.", nextState: "..#.."),
-            (currentState: @"\.#\.\.\.", nextState: "..#.."),
-            (currentState: @"\.#\.#\.", nextState: "..#.."),
-            (currentState: @"\.#\.##", nextState: "..#.."),
-            (currentState: @"\.##\.\.", nextState: "..#.."),
-            (currentState: @"\.####", nextState: "..#.."),
-            (currentState: @"#\.#\.#", nextState: "..#.."),
-            (currentState: @"#\.###", nextState: "..#.."),
-            (currentState: @"##\.#\.", nextState: "..#.."),
-            (currentState: @"##\.##", nextState: "..#.."),
-            (currentState: @"###\.\.", nextState: "..#.."),
-            (currentState: @"###\.#", nextState: "..#.."),
-            (currentState: @"####\.", nextState: "..#..")
+            "...##",
+            "..#..",
+            ".#...",
+            ".#.#.",
+            ".#.##",
+            ".##..",
+            ".####",
+            "#.#.#",
+            "#.###",
+            "##.#.",
+            "##.##",
+            "###..",
+            "###.#",
+            "####."
+        };
+
+        static readonly HashSet<string> RULE_SET = new HashSet<string>()
+        {
+            //".#.#.",
+            "...#.",
+            //"..##.",
+            //"....#",
+            "##.#.",
+            ".##.#",
+            ".####",
+            "#.#.#",
+            "#..#.",
+            //"##..#",
+            //"#####",
+            //"...##",
+            //".#...",
+            "###..",
+            //"#..##",
+            //"#...#",
+            ".#..#",
+            //".#.##",
+            "#.#..",
+            //".....",
+            //"####.",
+            "##.##",
+            "..###",
+            //"#....",
+            //"###.#",
+            ".##..",
+            "#.###",
+            //"..#.#",
+            ".###.",
+            "##...",
+            "#.##.",
+            "..#.."
         };
 
         static void Main(string[] args)
         {
-            string finalState = INITIAL_STATE;
-            Console.WriteLine(finalState);
+            //HashSet<string> rules = TEST_RULE_SET;
+            //string initialState = TEST_INITIAL_STATE;
 
-            for (int i = 0; i < 20; i++)
+            HashSet<string> rules = RULE_SET;
+            string initialState = INITIAL_STATE;
+
+            int expandedLength = (int)(initialState.Length * 0.5);
+
+            string currentState = new string('.', expandedLength) + initialState + new string('.', expandedLength);
+
+            Console.WriteLine(currentState);
+
+            int maxIteration = 20;
+            for (int t = 0; t < maxIteration; t++)
             {
-                List<int> plantIndices = new List<int>();
-                foreach (var rule in rules)
+                List<int> plantIndices = new List<int>();   
+                
+                int start = Math.Max(0, currentState.IndexOf('#') - 4);
+                int end = Math.Min(currentState.Length, currentState.LastIndexOf('#') + 4);
+
+                for (int i = start; i < end - 2; i++)
                 {
-                    Regex ruleMatcher = new Regex(rule.currentState);
-                    foreach (Match match in ruleMatcher.Matches(finalState))
+                    string pattern = new string(new char[] { currentState[i - 2], currentState[i - 1], currentState[i], currentState[i + 1], currentState[i + 2], });
+                    if (rules.Contains(pattern))
                     {
-                        if (match.Success)
-                        {
-                            plantIndices.Add(match.Index + 2);
-                        }
-                    }
-                }
-                char[] stateArray = new string('.', finalState.Length).ToCharArray();
+                        plantIndices.Add(i);
+                    }                    
+                }                                
+
+                char[] stateArray = new string('.', currentState.Length).ToCharArray();
                 for (int p = 0; p < plantIndices.Count; p++)
                 {
                     stateArray[plantIndices[p]] = '#';
                 }
-                finalState = new string(stateArray);
-                Console.WriteLine(finalState);
+                currentState = new string(stateArray);
+
+                Console.WriteLine(currentState);                                
             }
+                        
+            Console.WriteLine(currentState);
+
+            int result = 0;
+            int firstPlanIndex = currentState.IndexOf('#');
+            int lastPlanIndex = currentState.LastIndexOf('#');
+            for (int i = firstPlanIndex; i <= lastPlanIndex; i++)
+            {
+                if (currentState[i] == '#')
+                {
+                    result += i - expandedLength;
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
